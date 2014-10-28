1b69cc789c04d564df9bedf317bc8c4348c2ff42
Jeffrey Bergier <support@saturdayapps.com>
2014-10-28 04:22:13 +0000

    added crashlytics.
    git-svn-id: https://www.jeffburg.com/saturdayappssvn/GratuitousSwift@18 bd13cce4-38e5-42be-9df0-bbfa65dd9970

--- GratuitousSwift/AppDelegate.swift
+++ GratuitousSwift/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //crashlytics intializer
+        Fabric.with([Crashlytics()])
         
         //prepare currency formatter and nsuserdefaults
         self.prepareUserDefaults()
@@ -78,6 +83,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        Crashlytics.sharedInstance().crash()
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
