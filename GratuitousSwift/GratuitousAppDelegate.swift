975b7ba6286bb1961dd1d4a54089f981abfe2eb9
Jeffrey Bergier <support@saturdayapps.com>
2014-11-22 22:38:04 +0000

    finally got my version control under control.
    git-svn-id: https://www.jeffburg.com/saturdayappssvn/GratuitousSwift@87 bd13cce4-38e5-42be-9df0-bbfa65dd9970

--- GratuitousSwift/GratuitousAppDelegate.swift
+++ GratuitousSwift/GratuitousAppDelegate.swift
@@ -77,33 +77,3 @@ class GratuitousAppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-class FakeViewController: UIViewController {
-    //
-    //
-    //this entire class is needed just to fix the bug where launching in landscape breaks autolayout
-    //
-    //
-    
-    var realViewController: TipViewController?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = GratuitousUIConstant.darkBackgroundColor()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        if let realViewController = self.realViewController {
-            self.presentViewController(realViewController, animated: false, completion: nil)
-        }
-    }
-    
-    override func supportedInterfaceOrientations() -> Int {
-        return UIInterfaceOrientation.Portrait.rawValue
-    }
-    
-    override func shouldAutorotate() -> Bool {
-        return false
-    }
-}
-
