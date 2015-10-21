656ce7a604ec1fdc2cfe8375400a91703bc8a8d3
Jeffrey Bergier <jeffburg@jeffburg.com>
2015-10-20 22:10:15 -0700

    whoops. Don't give away features for free.

--- GratuitousSwift Watch WatchKit Extension/PickerInterfaceController.swift
+++ GratuitousSwift Watch WatchKit Extension/PickerInterfaceController.swift
@@ -284,7 +284,7 @@ final class PickerInterfaceController: WKInterfaceController {
     }
     
     @objc private func splitTipMenuButtonTapped() {
-        if true == true { //self.applicationPreferences.splitBillPurchased == true {
+        if self.applicationPreferences.splitBillPurchased == true {
             self.presentControllerWithName("SplitTotalInterfaceController", context: .None)
         } else {
             self.presentControllerWithName("SplitBillPurchaseInterfaceController", context: .None)
