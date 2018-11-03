6defdd1755ad02df6d3255d934b300607bd84641
Jeffrey Bergier <jeffburg@jeffburg.com>
2018-11-02 18:55:58 -0700

    fixed one startup warning

---
+++ GratuitousSwift Watch WatchKit Extension/ExtensionDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  ExtensionDelegate.swift
+//  GratuityWatch Extension
+//
+//  Created by Jeffrey Bergier on 11/1/18.
+//  Copyright © 2018 SaturdayApps. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate { }
