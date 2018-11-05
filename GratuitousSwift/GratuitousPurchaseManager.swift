d3afd3a4038ebe30e76968a350e0b0db07436c5d
Jeffrey Bergier <jeffburg@jeffburg.com>
2018-11-04 17:22:52 -0800

    tested IAP. Seems to work.

--- GratuitousSwift/GratuitousPurchaseManager.swift
+++ GratuitousSwift/GratuitousPurchaseManager.swift
@@ -7,6 +7,8 @@
 //
 
 final class GratuitousPurchaseManager: JSBPurchaseManager {
+
+    static let splitBillPurchaseIdentifier = "com.saturdayapps.gratuity.splitbillpurchase"
     
     override var Scej9Uj9vIrth8Ev7quaG9vob6iP8buK5ferS8yoak3Fots5El: String {
         let bytes: [CChar] = [0x63, 0x6f, 0x6d, 0x2e, 0x73, 0x61, 0x74, 0x75, 0x72, 0x64, 0x61, 0x79, 0x61, 0x70, 0x70, 0x73, 0x2e, 0x47, 0x72, 0x61, 0x74, 0x75, 0x69, 0x74, 0x79]
@@ -23,16 +25,19 @@ final class GratuitousPurchaseManager: JSBPurchaseManager {
     }
     
     func splitBillReceipt() -> RMAppReceiptIAP? {
-        if let receipt = RMAppReceipt.bundle(), let purchases = receipt.inAppPurchases, self.verifyAppReceiptAgainstAppleCertificate() == true {
-            for purchase in purchases {
-                if purchase.productIdentifier == GratuitousPurchaseManager.splitBillPurchaseIdentifier {
-                    return purchase
-                }
+        guard
+            let receipt = RMAppReceipt.bundle(),
+            let purchases = receipt.inAppPurchases,
+            self.verifyAppReceiptAgainstAppleCertificate() == true
+        else { return nil }
+        for purchase in purchases {
+            if purchase.productIdentifier == GratuitousPurchaseManager.splitBillPurchaseIdentifier {
+                return purchase
             }
         }
-        return .none
+        return nil
     }
-    
+
     func verifySplitBillPurchaseTransaction() -> Bool {
         if let _ = self.splitBillReceipt() {
             return true
@@ -40,7 +45,5 @@ final class GratuitousPurchaseManager: JSBPurchaseManager {
             return false
         }
     }
-    
-    static let splitBillPurchaseIdentifier = "com.saturdayapps.gratuity.splitbillpurchase"
 }
 
